Fixed backpropag from 2nd seminar. Finished task for 3rd seminar.

navy_backpropag.py
@@ -13,15 +13,15 @@ def count_error(expected, predicted):
 
 def dot_product(input, bias):
     # print(sum([item[0]*item[1] + bias for item in input]))
-    return sum([item[0] * item[1] for item in input])
+    return sum([item[0] * item[1] + bias for item in input])
 
 def predict(point, bias, weightsH, weightsO):
     inpts = [[[point[0],weightsH[0]], [point[1],weightsH[1]]], [[point[0],weightsH[2]], [point[1],weightsH[3]]]]
     outsH = []
     outsH.append(sigm(dot_product(inpts[0], bias[0])))
-    outsH.append(sigm(dot_product(inpts[1],bias[0])))
+    outsH.append(sigm(dot_product(inpts[1],bias[1])))
     inpts = [[outsH[0],weightsO[0]], [outsH[1],weightsO[1]]]
-    out = sigm(dot_product(inpts,bias[1]))
+    out = sigm(dot_product(inpts,bias[2]))
     return outsH, out
 
 
@@ -38,7 +38,7 @@ def predict(point, bias, weightsH, weightsO):
     #weightsH = [weightH*learnConst*dH*inpt for weightH, inpt in zip(weightsH,inputs)]
     return weightsO, weightsH"""
 
-def backpropagation(error, outO, outsH, weightsO, weightsH, learnConst, inputs):
+def backpropagation(error, outO, outsH, weightsO, weightsH, learnConst, inputs, biases):
     dO = error * sigm_(outO)
     dH = [dO*weightO*sigm_(outH) for weightO, outH in zip(weightsO, outsH)]
     weightsO = [weightO+learnConst*dO*outH for weightO, outH in zip(weightsO, outsH)]
@@ -47,7 +47,14 @@ def backpropagation(error, outO, outsH, weightsO, weightsH, learnConst, inputs):
     for i in [2,3]:
         weightsH[i] += learnConst * dH[1] * inputs[i % 2]
 
-    return weightsO, weightsH
+    """biases[0] = adapt_bias(biases[0],error,learnConst)
+    biases[1] = adapt_bias(biases[1],error,learnConst)"""
+
+    biases[0] = adapt_bias(biases[0], dH[0], learnConst)
+    biases[1] = adapt_bias(biases[1], dH[1], learnConst)
+    biases[2] = adapt_bias(biases[2], dO, learnConst)
+
+    return weightsO, weightsH, biases
 
 def adapt_bias(bias, error, leaning_const):
     return bias + error*leaning_const
@@ -59,21 +66,23 @@ def start_prog(epochs):
     learn_const = 0.2
     weightsH = [np.random.uniform() for i in range(4)]
     weightsO = [np.random.uniform() for i in range(2)]
-    biases = [1,1]
+    biases = [1,1,1]
     for epoch in range(epochs):
         error_sum = 0
         for index, point in enumerate(inputs):
             outsH, out = predict(point, biases, weightsH, weightsO)
             error = count_error(xored[index], out)
             error_sum += math.fabs(error)
-            weightsO, weightsH = backpropagation(error, out, outsH, weightsO, weightsH, learn_const, point)
-        if error_sum < 0.5:
+            weightsO, weightsH, biases = backpropagation(error, out, outsH, weightsO, weightsH, learn_const, point, biases)
+        if error_sum < 0.4:
             print("{}: {}".format(epoch,error_sum))
-
+    print(weightsH)
+    print(weightsO)
+    print(biases)
     for index, point in enumerate(inputs):
         #print(predict(point, biases, weightsH, weightsO))
         print("Point {} is expeted to be {} and was predicted as {}".format(point, xored[index], predict(point, biases, weightsH, weightsO)[1]))
 
 
-start_prog(25000)
+start_prog(10000)
 

navy_backpropag_evolution.py
@@ -10,7 +10,7 @@ def count_error(expected, predicted):
 
 def dot_product(input, bias):
     # print(sum([item[0]*item[1] + bias for item in input]))
-    return sum([item[0] * item[1] for item in input])
+    return sum([item[0] * item[1] + bias for item in input])
 
 def predict(point, weightsH, weightsO, bias=(1,1)):
     inpts = [[[point[0],weightsH[0]], [point[1],weightsH[1]]], [[point[0],weightsH[2]], [point[1],weightsH[3]]]]
@@ -33,10 +33,16 @@ def normal_distribution(mean, scale, min_max=None):
 
     return element
 
-def generate_identity(dim):
+def generate_identity(dim, minimum=None, maximum=None, boundaries=None):
+    if boundaries is not None:
+        minimum = boundaries[0]
+        maximum = boundaries[1]
+    else:
+        if minimum is None or maximum is None:
+            raise ValueError("No minimum or maximum was set")
     res = []
     for i in range(dim):
-        res.append(np.random.uniform())
+        res.append(np.random.uniform(minimum, maximum))
     return res
 
 def generate_identity_with_normal(prev, scale, min_max=None):
@@ -49,7 +55,7 @@ def generate_identity_with_normal(prev, scale, min_max=None):
 
 def hill_climbing(minimum, maximum, dim, cycles, num_of_identities, scale, test_func):
     all_res = []
-    best = generate_identity(dim)
+    best = generate_identity(dim, boundaries=[minimum,maximum])
     fitness = test_func(best)
     best_tmp = []
     for i in range(cycles):
@@ -57,12 +63,13 @@ def hill_climbing(minimum, maximum, dim, cycles, num_of_identities, scale, test_
             tmp = generate_identity_with_normal(best, scale, [minimum, maximum])
             all_res.append(tmp)
             fit_tmp = test_func(tmp)
-            print(fit_tmp)
             if fit_tmp < fitness:
+                print(fit_tmp)
                 fitness = fit_tmp
                 best_tmp = tmp
         if len(best_tmp) == len(best):
             best = best_tmp
+    print(best)
     return best
 
 def test_function(weights):
@@ -70,19 +77,80 @@ def test_function(weights):
     xored = [0, 1, 1, 0]
     error_sum = 0
     for index, point in enumerate(inputs):
-        _, out = predict(point,weights[:4], weights[4:])
+        _, out = predict(point,weights[:4], weights[4:6],weights[6:])
         error = count_error(xored[index], out)
         error_sum += math.fabs(error)
     return error_sum
 
+
+def particle_swarm(boundaries, dim, count_of_particles, iterations, test_function, c1=0.2, c2=0.2):
+    particles = []
+    particle_speeds =[]
+    fitness = []
+    personal_best = []
+    points_to_draw = [[] for x in range(iterations)]
+    for i in range(count_of_particles):
+        particles.append(generate_identity(dim, boundaries=boundaries))
+        personal_best.append(particles[i].copy())
+        particle_speeds.append(generate_speed(dim, boundaries=boundaries))
+        fitness.append(test_function(particles[i]))
+    global_best = particles[fitness.index(min(fitness))]
+
+    for i in range(iterations):
+        for j in range(len(particles)):
+            particle_speeds[j] = count_speed(dim, particles[j], particle_speeds[j], personal_best[j],global_best,c1,c2,count_w(j,i+1))
+            particles[j] = count_new_pos(dim, particles[j], particle_speeds[j],boundaries)
+            points_to_draw[i].append(particles[j])
+            fitness[j] = test_function(particles[j])
+            p_best = test_function(personal_best[j])
+            if fitness[j] < p_best:
+                #print(fitness[j])
+                personal_best[j] = particles[j].copy()
+            if p_best < test_function(global_best):
+                global_best = personal_best[j]
+
+    print(global_best)
+    return global_best
+
+
+def generate_speed(dim, boundaries):
+    max_speed = (math.fabs(boundaries[0])+math.fabs(boundaries[1]))/20
+    return generate_identity(dim,boundaries=[0,max_speed])
+
+
+def count_w(iteration, migration):
+    w_start = 0.9
+    w_end = 0.4
+    return w_start - ((w_start-w_end)*iteration)/migration
+
+#TODO pokud rychlost prekroci vMax vygenerovat pro danou dimenzi nahodne novou
+def count_speed(dim, position, prev_speed, personal_best, global_best, c1, c2,w):
+    next_speed = []
+    for i in range(dim):
+        next_speed.append(w*prev_speed[i]+ c1*np.random.uniform()*(personal_best[i]-position[i]) + c2*np.random.uniform()*(global_best[i]-position[i]))
+    return next_speed
+
+def count_new_pos(dim, position, speed, boundaries):
+    new_pos = []
+    for i in range(dim):
+        value = position[i] + speed[i]
+        value = check_boundaries(boundaries,value)
+        new_pos.append(value)
+    return new_pos
+
+def check_boundaries(boundaries, value):
+    if value < boundaries[0] or value > boundaries[1]:
+        value = np.random.uniform(boundaries[0], boundaries[1])
+    return value
+
 def start_prog():
     inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
     xored = [0, 1, 1, 0]
-    weights = hill_climbing(-30.0,30.0,6,500,100,10, test_function)
+    weights = particle_swarm([-30,30],8,100,100,test_function)#hill_climbing(-30.0,30.0,8,500,500,8.0, test_function)
 
 
     for index, point in enumerate(inputs):
         #print(predict(point, biases, weightsH, weightsO))
-        print("Point {} is expeted to be {} and was predicted as {}".format(point, xored[index], predict(point,weights[:4], weights[4:])[1]))
+        print("Point {} is expeted to be {} and was predicted as {}".format(point, xored[index], predict(point,weights[:4], weights[4:6],weights[6:])[1]))
 
 start_prog()