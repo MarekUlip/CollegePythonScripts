6aecf47803bbe6a85ae7ac55cd2c913dc2237ed6
Marek Ulip <marek.ulip@gmail.com>
2019-03-20 22:24:29 +0100

    Working solution after 6th seminar.

--- 5cvic.py
+++ 5cvic.py
@@ -1,11 +1,14 @@
 import csv
 import math
+import copy
 
 matrix = [[0 for col in range(34)] for row in range(34)]
 nodes = []
+edges = []
 with open('1KarateClub.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
     for row in readCSV:
+        edges.append(row.copy())
         row[0] = int(row[0])
         row[1] = int(row[1])
         #original_data.append(row)
@@ -16,8 +19,8 @@ with open('1KarateClub.csv') as csvfile:
         nodes.append(row)
 
 def count_similarity(matrix):
-    sim_matrix = [[0 for col in range(34)] for row in range(34)]
     n = len(matrix)
+    sim_matrix = [[0 for col in range(n)] for row in range(n)]
     for i in range(n):
         a_nodes = []
         shared_nodes = []
@@ -37,29 +40,76 @@ def count_similarity(matrix):
             sim_matrix[i][j] = len(shared_nodes)/ math.sqrt(len(a_nodes)*len(b_nodes))
     return sim_matrix  
 
+def find_index_in_sub_cluster(cluster, a, b = None):
+    a_cluster_index = b_cluster_index = -1
+    if b is None:
+        b_cluster_index = -2
+    for index, c in enumerate(cluster):
+        if type(c) is list:
+            if a in c:
+                a_cluster_index = index
+                continue
+            if b in c: #Maybe check for none
+                b_cluster_index = index
+        if a_cluster_index != -1 and b_cluster_index != -1:
+            break
+    return [a_cluster_index, b_cluster_index]
+    
+    
+
 def clustering(matrix):
     clustered_nodes = []
     clusters = []
-    comunity_size = 0
-    while comunity_size < len(matrix):
+    cluster = [i for i in range(len(matrix))]
+    cluster_count = len(matrix)
+    while cluster_count > 1:
+        print(cluster_count)
         a, b = simple_linkage(matrix)
-        if a in clustered_nodes:
+        print("a: {}, b: {}".format(a,b))
+        if a in clustered_nodes and b in clustered_nodes:
+            """a_cluster_index = b_cluster_index = -1
+            for index, c in enumerate(cluster):
+                if type(c) is list:
+                    if a in c:
+                        a_cluster_index = index
+                        continue
+                    if b in c:
+                        b_cluster_index = index
+                if a_cluster_index != -1 and b_cluster_index != -1:
+                    break"""
+            a_cluster_index, b_cluster_index = find_index_in_sub_cluster(cluster, a, b)
+            cluster[a_cluster_index].extend(cluster[b_cluster_index])
+            update_matrix(cluster[a_cluster_index], matrix)
+            cluster.remove(cluster[b_cluster_index])
+        elif a in clustered_nodes:
+            a_cluster_index = find_index_in_sub_cluster(cluster, a)[0]
+            cluster[a_cluster_index].append(b)
+            update_matrix(cluster[a_cluster_index], matrix)
+            cluster.remove(b)
+            clustered_nodes.append(b)
+        elif b in clustered_nodes:
+            b_cluster_index = find_index_in_sub_cluster(cluster,b)[0]
+            cluster[b_cluster_index].append(a)
+            update_matrix(cluster[b_cluster_index], matrix)
+            cluster.remove(a)
+            clustered_nodes.append(a)
+        else:
+            new_cluster_index = cluster.index(a)
+            cluster[new_cluster_index] = [a,b]
+            update_matrix(cluster[new_cluster_index], matrix)
+            #cluster.remove(a)
+            cluster.remove(b)
+            clustered_nodes.append(a)
+            clustered_nodes.append(b)
+        clusters.append(copy.deepcopy(cluster))
+        cluster_count -= 1
+    print(len(clusters))
+    return clusters
 
-def go_deeper(depth, cluster, element):
-    if all(isinstance(x,float) for x in cluster):
-        return [depth, element in cluster]
-    else:
-        if isinstance(cluster[1],float):
-            return [depth, cluster[1] == element]
-        go_deeper(depth+1, cluster[0],element)
-        return go_deeper(depth+1, )
-
-def find_biggest_cluster_with_element(element, clusters):
-    for cluster in clusters:
-        depth = 0
-        while not all(isinstance(x,float), cluster):
-            depth+=1
-
+def update_matrix(community, matrix):
+    for i in community:
+        for j in community:
+            matrix[i][j] = 0
         
 
 def simple_linkage(matrix):
@@ -69,9 +119,40 @@ def simple_linkage(matrix):
     sec_vertex_num = matrix[vertex_num].index(max_val)
     return [vertex_num, sec_vertex_num]
 
+def complete_linkage(matrix):
+    closest = [min(row) for row in matrix]
+    max_val = min(closest)
+    vertex_num = closest.index(max_val)
+    sec_vertex_num = matrix[vertex_num].index(max_val)
+    return [vertex_num, sec_vertex_num]
+
+def output_cluster(cluster, num_of_clusters):
+    to_output = ["nodedef>name VARCHAR, label VARCHAR, color VARCHAR\n"]
+    edge_part = "edgedef> node1,node2\n"
+    cluster_less_color = "#FF0000"
+    colors = ["#8B008B", "#A9A9A9", "#1E90FF", "#F08080", "#7B68EE", "#2F4F4F", "#A0522D", "#483D8B", "#48D1CC", "#00FF00", "#B8860B", ]
+    for sub_clust in cluster:
+        if type(sub_clust) is list:
+            color = colors.pop()
+            for item in sub_clust:
+                to_output.append("{},\"{}\",{}\n".format(item+1,item+1,color))
+        else:
+            to_output.append("{},\"{}\",{}\n".format(sub_clust+1,sub_clust+1,cluster_less_color))
+    to_output.append(edge_part)
+    for edge in edges:
+        to_output.append("{},{}\n".format(edge[0],edge[1]))
+    with open("clusters-{}.gdf".format(num_of_clusters), "w") as f:
+        for item in to_output:
+            f.write(item)
+
+
 similarity_matrix = count_similarity(matrix)
 similarity_matrix_work = similarity_matrix.copy()
-simple_linkage(similarity_matrix)      
+clusters = clustering(similarity_matrix_work)
+[print("{}: {}".format(index,row)) for index,row in enumerate(clusters)]
+clust_num = 19
+output_cluster(clusters[clust_num],clust_num+1)
+#simple_linkage(similarity_matrix)      
 #[print(row) for row in count_similarity(matrix)]
 
     
---
+++ 6cvic.py
@@ -0,0 +1,67 @@
+import csv
+import math
+import copy
+
+matrix = [[0 for col in range(34)] for row in range(34)]
+nodes = []
+edges = []
+with open('1KarateClub.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=';')
+    for row in readCSV:
+        edges.append(row.copy())
+        row[0] = int(row[0])
+        row[1] = int(row[1])
+        #original_data.append(row)
+        col = int(row[1])-1
+        row = int(row[0])-1
+        matrix[row][col] = 1
+        matrix[col][row] = 1
+        nodes.append(row)
+
+
+def k_core(k, matrix):
+    k_classes = {}
+    n = len(matrix[0])
+    node_degrees = [sum(row) for row in matrix]
+    #for i in range(1,k+1):
+    
+    k_classes = []
+    removed = 999
+    while removed > 0:
+        removed = 0
+        for vertex, node_degree in enumerate(node_degrees):
+            if node_degree < k:
+                #matrix[vertex] = [0 for i in range(n)]
+                for index, col in enumerate(matrix[vertex]):
+                    if col == 1:
+                        removed += 1
+                        matrix[vertex][index] = matrix[index][vertex] = 0
+        node_degrees = [sum(row) for row in matrix]
+    print(node_degrees)
+    #for index, node_degree in enumerate(node_degrees):
+    #    if node_degree >= i:
+    #        k_classes[i].append(index)
+    for index, degree in enumerate(node_degrees):
+        if degree > 0:
+            k_classes.append(index)
+    
+    """for i in range(1,k):
+        #print(len(k_classes[i]))
+        to_remove = []
+        for item in k_classes[i]:            
+            if item in k_classes[i+1]:
+                to_remove.append(item)
+        for item in to_remove:
+            k_classes[i].remove(item)
+        #print(counter)"""
+
+    print(k_classes)
+    return k_classes
+    
+#[print(row) for row in matrix]
+k_classes = []
+for i in range(1,5):
+    k_classes.append(k_core(i,matrix.copy()))
+
+for clas in k_classes:
+    print(len(clas))
